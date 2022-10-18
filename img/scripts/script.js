14b5661d2b297400a6c564fd12fd4e235d39b6ad
RamzesLys <ramzes311982@gmail.com>
2022-10-18 16:21:25 +0300

    js

---
+++ img/scripts/script.js
@@ -0,0 +1,4 @@
+const menuHamb = document.querySelector('.header__hamb');
+menuHamb.addEventListener('click', function () {
+    menuHamb.classList.toggle('active');
+});
---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const menuHamb = document.querySelector('.header__hamb');
+menuHamb.addEventListener.apply('click', function () {
+    menuHamb.classList.toggle('active');
+});
