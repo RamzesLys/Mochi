cf7526ebd98f9f1602c7445b2881882045df09f5
RamzesLys <ramzes311982@gmail.com>
2023-01-04 22:17:23 +0200

    final

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,14 @@ const menuHidden = document.querySelector('.header__menu-hidden');
 menuHamb.addEventListener('click', function () {
     menuHamb.classList.toggle('active');
     menuHidden.classList.toggle('active');
-})
+});
+
+const dropdownBtn = document.querySelector('.sticky__btn');
+const dropdownMenu = document.querySelector('.sticky__dropdown-menu');
+
+dropdownBtn.addEventListener('click', function () {
+    dropdownBtn.classList.toggle('active');
+    dropdownMenu.classList.toggle('active');
+    
+});
+
